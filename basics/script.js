9115696e2fe7dee7b19915d506a9b953abd0185e
egor <maiorov.prog@gmail.com>
2016-11-03 23:43:57 +0200

    changes

--- basics/script.js
+++ basics/script.js
@@ -1,15 +1,26 @@
-// The Module Pattern
+// Code goes here
 
 (function(){
 
   var app = angular.module("gitHubViewer", [])
 
-var MainController = function(t) {
-  t.message="Hello Angular!!"
+
+
+var MainController = function(scope, http) {
+    var onComplete = function(response) {
+    scope.user = response.data
+  }
+
+  var onError = function(reason) {
+    scope.error_reason = reason
+  }
+
+  http.get("https://api.github.com/users/maiorovi").then(onComplete, onError)
+  scope.message="Hello Angular!!"
 }
 
   // here we ask angular to pass scope as a first parameter to function
   // which creates maincontroller regardless of funciton argument name
-  app.controller("MainController", ["$scope",MainController])
+  app.controller("MainController", ["$scope", "$http", MainController])
 
 }());
